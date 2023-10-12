c368634741e919e43fa43e21b8a5fd5aa1c265fa
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2023-10-12 16:14:36 -0300

    first commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="content">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Layout',
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    getPokemons() {
+
+      let limit = 30
+      let offset = 0;
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+        .then((response) => {
+          console.log(response.data); 
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  mounted() {
+    this.getPokemons();
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
