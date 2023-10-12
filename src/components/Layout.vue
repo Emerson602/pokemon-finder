6658d2bf497ac5911af8ba85c4a2f2e199030a6c
Emerson602 <wemerson.azevedo@iqb.ufal.br>
2023-10-12 18:11:13 -0300

    update

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="content">
-
+    <ul>
+      <li v-for="pokemon in pokemons.results" :key="pokemon.name">
+        {{ pokemon.name }} 
+        <img :src="getPokemonImage(pokemon)" :alt="pokemon.name">       
+      </li>
+    </ul> 
   </div>
 </template>
 
@@ -10,32 +15,38 @@ import axios from 'axios';
 export default {
   name: 'Layout',
   data() {
-    return {
-      
+    return {      
+      pokemons: [],
     };
   },
   methods: {
     getPokemons() {
-
-      let limit = 30
-      let offset = 0;
+      let limit = 150;
       axios
-        .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
         .then((response) => {
-          console.log(response.data); 
+          this.pokemons = response.data;
         })
         .catch((error) => {
           console.error(error);
         });
     },
+    
+    getPokemonImage(pokemon) {
+      const id = this.get_id(pokemon);
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`;
+    },
+
+    get_id(pokemon) {
+      const urlParts = pokemon.url.split('/');
+      return urlParts[urlParts.length - 2];
+    }
   },
   mounted() {
-    this.getPokemons();
+    this.getPokemons();     
   },
 };
 </script>
 
-
 <style scoped>
-
 </style>
